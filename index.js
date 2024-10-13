4e4f2d07ceb813a33db297220cbc658b63976bc8
Jhadevloper4035 <navrojjha21@gmail.com>
2024-10-14 02:27:55 +0530

    all work done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let db;
 
 async function initDb() {
   db = await open({
-    filename: './BD4.1_HW1/books_database.sqlite',
+    filename: 'database.sqlite',
     driver: sqlite3.Database,
   });
   return db;
@@ -22,15 +22,52 @@ async function initDb() {
 initDb();
 
 async function fetchAllBooks() {
-  const query = 'SELECT * FROM books';
-  const response = await db.all(query);
+  let query = 'SELECT * FROM books';
+  let response = await db.all(query);
   return { books: response };
 }
 
-app.get('/', async (req, res) => {
+async function fetchBooksByAuthor(author){
+  let query = "SELECT * FROM books WHERE author = ? "
+  let response = await db.all(query,[author]);
+  return {books : response}
+}
+
+async function fetchBooksByGenre(genre){
+  let query = "SELECT * FROM books WHERE genre = ? "
+  let response = await db.all(query,[genre]);
+  return{books : response}
+}
+
+async function fetchBooksByPublicationYear(publication_year){
+  let query = 'SELECT * FROM books WHERE publication_year = ? '
+  let response = await db.all(query,[publication_year]);
+  return {books : response}
+}
+
+app.get('/books', async (req, res) => {
   const result = await fetchAllBooks();
   res.status(200).json(result);
 });
+
+app.get('/books/author/:author' , async(req,res)=>{
+  let author = req.params.author;
+let result = await fetchBooksByAuthor(author);
+res.status(200).json(result)
+})
+
+app.get('/books/genre/:genre' , async(req,res)=>{
+  let genre = req.params.genre;
+  let result = await fetchBooksByGenre(genre);
+  res.status(200).json(result);
+})
+
+app.get('/books/publication_year/:year' , async(req,res)=>{
+  let publication_year = parseInt(req.params.year);
+  let result = await fetchBooksByPublicationYear(publication_year)
+  res.status(200).json(result);
+})
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
